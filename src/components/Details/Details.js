c8202f7ce89ecbab1af5dae63ae2204ec042b485
azad-ali786 <azad_ug@cse.nits.ac.in>
2021-05-01 16:27:58 +0530

    Adaptive design Done

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -1,17 +1,19 @@
-import React from 'react'
-import calendar from '../images/calender.png'
+import React from "react";
+import calendar from "../images/calender.png";
 import "../Details/Details.css";
-const Details = ({name,description,language,date,link}) => {
-    return (
-        <div className="cards">
-        <a href={link}><h1 className="language">{language}</h1></a>
-        <h1>{name}</h1>
-        <h2>{description}</h2>
-        <div className="date-details">
-            <img src={calendar} alt=""/>
-            <h3>{date}</h3>
-        </div>
+const Details = ({ name, description, language, date, link }) => {
+  return (
+    <div className="cards">
+      <a href={link}>
+        <h1 className="language">{language}</h1>
+      </a>
+      <h1>{name}</h1>
+      <h2>{description}</h2>
+      <div className="date-details">
+        <img src={calendar} alt="" />
+        <h3>{date}</h3>
+      </div>
     </div>
-    );
-}
+  );
+};
 export default Details;
